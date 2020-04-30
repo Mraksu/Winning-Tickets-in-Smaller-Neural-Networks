a7ff2132764c70872eea98fce6f209f66b94094c
Murat <aksumurat@yahoo.com>
2020-04-30 12:47:42 +0300

    Create model.py

---
+++ model.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf
+import numpy as np
+import tensorflow_model_optimization as tfmot
+from tensorflow_model_optimization.sparsity import keras as sparsity
+from tensorflow.keras import models, layers, datasets
+from tensorflow.keras.layers import Dense, Flatten, Reshape, Input, InputLayer
+from tensorflow.keras.models import Sequential, Model
+
+
+# In[2]:
+
+
+def pruned_nn(pruning_params,first_layer=300,second_layer=100):
+    """
+    Function to define a 300 100 dense
+    fully connected architecture for MNIST
+    classification
+    """
+    prun=pruning_params[0]
+    prun_out=pruning_params[1]
+    
+    pruned_model = Sequential()
+    
+    pruned_model.add(
+        sparsity.prune_low_magnitude(
+            Dense(units = first_layer, activation = 'relu',
+                  kernel_initializer = tf.keras.initializers.glorot_normal(),
+                  input_shape = (784,)
+                 ), 
+            **prun)
+    )
+
+    pruned_model.add(
+        sparsity.prune_low_magnitude(
+            Dense(
+                units = second_layer, activation = 'relu',
+                kernel_initializer = tf.keras.initializers.glorot_normal()
+            ),
+            **prun))
+    
+    pruned_model.add(
+        sparsity.prune_low_magnitude(
+            Dense(
+                units = 10, activation = 'softmax'
+            ),
+            **prun_out))
+    
+    
+    pruned_model.compile(
+        loss = tf.keras.losses.categorical_crossentropy,
+        # 0.0012 learning rate with Adam optimizer is used in the original paper
+        optimizer=tf.keras.optimizers.Adam(0.0012),
+        metrics = ['accuracy'])
+
+    return pruned_model
+
