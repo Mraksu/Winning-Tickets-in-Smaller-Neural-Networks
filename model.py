0c6fc68acac4f4edaecd3bb37988a4dfa8d5a332
Murat <aksumurat@yahoo.com>
2020-05-14 23:48:11 +0300

    Update model.py

--- model.py
+++ model.py
@@ -3,6 +3,8 @@
 
 # In[1]:
 
+'''This file is for building the model for the experiments.
+Same model is used for every experiment for consistency.'''
 
 import tensorflow as tf
 import numpy as np
@@ -21,18 +23,28 @@ def pruned_nn(pruning_params,first_layer=300,second_layer=100):
     Function to define a 300 100 dense
     fully connected architecture for MNIST
     classification
+    Arguments:
+    pruning_params=dictionary for the desired pruning rate.
+    see the used_func.py for detailed function
+    first_layer= Desired unit size in first hidden layer
+    second_layer= Desired unit size in second hidden layer
+
+    returns a model
+
     """
+    #Using 2 different pruning rate, because the layer connected
+    #to the output pruned as half of the rest of the network
     prun=pruning_params[0]
     prun_out=pruning_params[1]
-    
+
     pruned_model = Sequential()
-    
+
     pruned_model.add(
         sparsity.prune_low_magnitude(
             Dense(units = first_layer, activation = 'relu',
                   kernel_initializer = tf.keras.initializers.glorot_normal(),
                   input_shape = (784,)
-                 ), 
+                 ),
             **prun)
     )
 
@@ -43,15 +55,15 @@ def pruned_nn(pruning_params,first_layer=300,second_layer=100):
                 kernel_initializer = tf.keras.initializers.glorot_normal()
             ),
             **prun))
-    
+
     pruned_model.add(
         sparsity.prune_low_magnitude(
             Dense(
                 units = 10, activation = 'softmax'
             ),
             **prun_out))
-    
-    
+
+
     pruned_model.compile(
         loss = tf.keras.losses.categorical_crossentropy,
         # 0.0012 learning rate with Adam optimizer is used in the original paper
@@ -59,4 +71,3 @@ def pruned_nn(pruning_params,first_layer=300,second_layer=100):
         metrics = ['accuracy'])
 
     return pruned_model
-
